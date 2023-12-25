eb22660533ebeff5e6a7c8724a4be400f31b9189
Tanishq Raj <tanishqraj306@gmail.com>
2023-12-26 02:49:06 +0530

    6 done

---
+++ 6.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <ctype.h>
+#define SIZE 50
+
+char s[SIZE];
+int top = -1;
+
+void push(char elem)
+{
+    s[++top] = elem;
+}
+
+char pop()
+{
+    return (s[top--]);
+}
+
+int pr(char elem)
+{
+    switch (elem)
+    {
+    case '(':
+        return 1;
+    case '+':
+    case '-':
+        return 2;
+    case '*':
+    case '/':
+    case '%':
+        return 3;
+    case '^':
+        return 4;
+    }
+}
+
+void main()
+{
+    char infix[50], postfix[50], ch, elem;
+    int i = 0, k = 0;
+    printf("Read Infix Expression: ");
+    scanf("%s", infix);
+
+    push('(');
+    while ((ch = infix[i++]) != '\0')
+    {
+        if (ch == '(')
+        {
+            push(ch);
+        }
+        else if (isalnum(ch))
+        {
+            postfix[k++] = ch;
+        }
+        else if (ch == ')')
+        {
+            while (s[top] != '(')
+            {
+                postfix[k++] = pop();
+            }
+            pop();
+        }
+        else
+        {
+            while (pr(s[top]) >= pr(ch))
+            {
+                postfix[k++] = pop();
+            }
+            push(ch);
+        }
+    }
+    while (s[top] != '(')
+    {
+        postfix[k++] = pop();
+    }
+    postfix[k] = '\0';
+    printf("\nGiven infix expression: %s", infix);
+    printf("\nPostfix Expression: %s\n", postfix);
+}
