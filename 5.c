f9fcf649e5da9d67f3f87e72f9d80e9bd8fc566f
Tanishq Raj <tanishqraj306@gmail.com>
2023-12-25 17:55:24 +0530

    Formatted 5

--- 5.c
+++ 5.c
@@ -1,55 +1,55 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define MAX 4
 
 int arr[MAX];
-int count=-1;
+int count = -1;
 
 void push()
 {
-    if (count==MAX)
+    if (count == MAX)
     {
         printf("Stack Overflow\n");
     }
-    else{
+    else
+    {
         count++;
-    printf("Enter data to push: ");
-    scanf("%d",&arr[count]);
-    
-}
+        printf("Enter data to push: ");
+        scanf("%d", &arr[count]);
+    }
 }
 
 void pop()
 {
-    if (count==-1)
+    if (count == -1)
     {
         printf("Stack Underflow\n");
     }
     else
     {
         int p;
-        p=arr[count];
-        arr[count]==NULL;
+        p = arr[count];
+        arr[count] == NULL;
         count--;
-        printf("Data Removed: %d\n",p);
+        printf("Data Removed: %d\n", p);
     }
 }
 
 void display()
 {
-    if (count==-1)
+    if (count == -1)
     {
         printf("Stack Underflow\n");
     }
-   else if(count==MAX+1)
+    else if (count == MAX + 1)
     {
         printf("Stack Overflow\n");
     }
     else
     {
-        for (int i=0;i<=count;i++)
+        for (int i = 0; i <= count; i++)
         {
-            printf("%d\t",arr[i]);
+            printf("%d\t", arr[i]);
         }
         printf("\n");
     }
@@ -57,29 +57,29 @@ void display()
 
 int main()
 {
-    while(1)
+    while (1)
     {
         printf("\n-------MENU------\n");
         printf("1. Push\n2. Pop\n3. Display\n4. Exit\n");
         int ch;
         printf("Enter choice: ");
-        scanf("%d",&ch);
+        scanf("%d", &ch);
 
         switch (ch)
         {
-            case 1:
-                push();
-                break;
-            case 2:
-                pop();
-                break;
-            case 3:
-                display();
-                break;
-            case 4:
-                exit(1);  
-            default:
-                printf("Invalid Input");
+        case 1:
+            push();
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(1);
+        default:
+            printf("Invalid Input");
         }
     }
     return 0;
